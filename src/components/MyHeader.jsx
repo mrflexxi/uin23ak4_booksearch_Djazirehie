6f7e7602a5c81c467b3df47612e6af43cc613d21
mrflexxi <ariendj@outlook.com>
2024-04-02 14:19:55 +0200

    Book Search V0.3
    Bokomslag er nå med for å få bedre oversikt og penere på BookCard. søking på SearchTerm mens man taster inn er borte og isteden må trykkes enten på enter eller søke-knappen for å starte søk.

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,5 +1,3 @@
-// MyHeader.jsx
-
 import React from 'react';
 import '../styles/App.scss';
 import '../styles/MyHeader.scss';
@@ -10,9 +8,7 @@ const MyHeader = ({
   handleSearchChange,
   searchLengthInfo,
   error,
-  noResults,
   isSearching,
-  searchResultsCount
 }) => {
   return (
     <header className="my-header">
@@ -27,15 +23,9 @@ const MyHeader = ({
         />
         <button type="submit" className="search-button">Search</button>
       </form>
-      {searchTerm.length > 0 && (
-        <div className="message-container">
-        {searchLengthInfo && <p className="short-length-message">Search term must be at least 3 characters long.</p>}
-        {error && <p className="message error">Error: {error}</p>}
-        {noResults && !isSearching && <p className="no-results-message">No results found. Please try with another search term.</p>}
-        {isSearching && <p className="fancy-loading-message">Please wait..., results for {searchTerm} are loading.</p>}
-        {!isSearching && searchResultsCount !== undefined && <p className="search-results-count">Found {searchResultsCount} results for '{searchTerm}'.</p>}
-      </div>
-      )}
+      {searchLengthInfo && <p className="short-length-message">Search term must be at least 3 characters long.</p>}
+      {error && <p className="message error">Error: {error}</p>}
+      {isSearching && <p className="fancy-loading-message">Please wait..., results for "{searchTerm}" are loading.</p>}
     </header>
   );
 }
